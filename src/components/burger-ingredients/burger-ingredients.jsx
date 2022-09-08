6343931441f0efda3c035ffe15d73d816f0923c1
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2022-09-08 15:14:51 +0300

    scroll smooth

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ArrayPropTypes } from "../../utils/proptypes";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { IngredientsType } from "../ingredients-type/ingredients-type";
 import styles from "./burger-ingredients.module.css";
@@ -11,30 +11,46 @@ export default function BurgerIngredients({ data }) {
   const mainArray = data.filter((element) => element.type === "main");
   const sauceArray = data.filter((element) => element.type === "sauce");
 
+  
+
+  const bunsRef = useRef(null);
+  const sauceRef = useRef(null);
+  const mainRef = useRef(null);
+
+
+  /*
   const onTabClick = (tab) => () => {
     setCurrent(tab);
     document.getElementById(tab).scrollIntoView({ behavior: "auto" });
-  };
+  };*/
+
+  
+
+const onTabClick = (tab, categoryRef) => () => {
+  setCurrent(tab);
+  const element = document.getElementById(tab);
+  if (element) element.scrollIntoView({ behavior: "smooth" });
+};
 
   return (
     <section className="mr-10">
       <p className="text text_type_main-large">Соберите бургер</p>
       <div className={`${styles.container} mt-5`}>
-        <Tab active={current === "bun"} onClick={onTabClick("bun")}>
+        <Tab active={current === "bun"} onClick={onTabClick("bun", bunsRef)}>
           Булки
         </Tab>
-        <Tab active={current === "sauce"} onClick={onTabClick("sauce")}>
+        <Tab active={current === "sauce"} onClick={onTabClick("sauce", sauceRef)}>
           Соусы
         </Tab>
-        <Tab active={current === "main"} onClick={onTabClick("main")}>
+        <Tab active={current === "main"} onClick={onTabClick("main", mainRef)}>
           Начинки
         </Tab>
       </div>
-      <section className={styles.tabs}>
+      <section className={styles.tabs} >
         <>
-          <IngredientsType id="bun" title="Булки" ingredients={bunsArray} />
-          <IngredientsType id="sauce" title="Соусы" ingredients={sauceArray} />
-          <IngredientsType id="main" title="Начинки" ingredients={mainArray} />
+          <IngredientsType id="bun" title="Булки" ingredients={bunsArray} ref={bunsRef}/>
+          <IngredientsType id="sauce" title="Соусы" ingredients={sauceArray} ref={sauceRef}/>
+          <IngredientsType id="main" title="Начинки" ingredients={mainArray}  ref={mainRef}/>
         </>
       </section>
     </section>
