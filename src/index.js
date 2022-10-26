a2d9cefb554700b536178a8c943cd0147c284174
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2022-10-26 17:33:18 +0300

    pages

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import thunk from "redux-thunk";
 import { compose, createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -21,11 +22,9 @@ const store = createStore(rootReducer, enhancer);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </Router>
 );
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import { Register } from "./registration/registration";
+import { Login } from './login/login';
+import { Profile } from "./profile/profile";
+import { ResetPassword } from "./resetPassword/resetPassword";
+import { ForgotPassword } from "./forgotPassword/forgotPassword";
+import { Orders } from './profile/orders/orders';
+import { NotFound404 } from './notFound404/notFound404';
+
+export {
+	Login,
+	Register,
+	ForgotPassword,
+	ResetPassword,
+	Profile,
+	Orders,
+	NotFound404
+};
